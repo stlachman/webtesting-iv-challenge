85cb9062c73b32f9f88523982bea4871566d993b
Seth Lachman <lachmanseth17@gmail.com>
2019-06-20 15:29:41 -0400

    Initial setup of server and npm packages.

---
+++ api/server.spec.js
@@ -0,0 +1,26 @@
+const server = require("./server");
+const request = require("supertest");
+
+describe("server", () => {
+  describe("GET /", () => {
+    it("responds with 200 ok", () => {
+      return request(server)
+        .get("/")
+        .expect(200);
+    });
+
+    it("responds with content type json", () => {
+      return request(server)
+        .get("/")
+        .expect("Content-Type", /json/i);
+    });
+
+    it("responds with the correct message ", () => {
+      return request(server)
+        .get("/")
+        .then(res => {
+          expect(res.body).toEqual({ message: "it's working" });
+        });
+    });
+  });
+});
